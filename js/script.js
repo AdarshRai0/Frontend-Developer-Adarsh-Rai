8cbdc503f85476aa0a03f9e111625700070a96dd
Adarshrai0 <adarshrai3011@gmail.com>
2022-05-04 18:30:54 +0530

    deleted animation

--- js/script.js
+++ js/script.js
@@ -22,18 +22,7 @@ if(scrollButton){
 
 
 
-// bg Animation effect
-function bgAnimationItems() {
-  const rows = 7,
-    cols = 10;
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      const div = document.createElement("div");
-      div.className = `col-${j + 1}`;
-      document.querySelector(".bg-animation-effect").appendChild(div);
-    }
-  }
-}
+
 //----------toogle body scrolling
 
 function toggleBodyScrolling() {
