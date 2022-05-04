6bf77f6a60a9c5eb178b9706543a77616da6111a
Adarshrai0 <adarshrai3011@gmail.com>
2022-05-04 20:11:39 +0530

    imlimented scroll to top button

--- js/script.js
+++ js/script.js
@@ -6,21 +6,28 @@ window.addEventListener("load", ()=>{
   }, 1000);
 })
 
-const scrollButton = document.querySelector(".scroll-top");
-if(scrollButton){
-  window.addEventListener('scroll', ()=> {
-    if(pageYOffset > (window.innerHeight * 1)){
-      scrollButton.style.display="flex";
-    }else{
-      scrollButton.style.display="none";
-    }
-  });
-  scrollButton.addEventListener("click", () => {
-    window.scrollTo(0, 0);
-  });
-}
 
 
+let calcScrollValue = () => {
+  let scrollProgress = document.getElementById("progress");
+  let progressValue = document.getElementById("progress-value");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  if (pos > 100) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient(#03cc65 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+};
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
 
 
 //----------toogle body scrolling
