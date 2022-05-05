0517c47ad0ec79f5a507bdf61565198b5ea3dfae
Adarshrai0 <adarshrai3011@gmail.com>
2022-05-05 17:09:49 +0530

    updated the time interval of portfolio items

--- js/script.js
+++ js/script.js
@@ -166,5 +166,5 @@ ScrollReveal().reveal(`.profile-pic`, { delay: 500 });
 ScrollReveal().reveal(`.profile__name`, { delay: 550 });
 ScrollReveal().reveal(`.portfolio-filter`, { delay: 650 });
 ScrollReveal().reveal(`.skills__data`, { delay: 600 });
-ScrollReveal().reveal(`.skills__content`, { delay: 700 });
-ScrollReveal().reveal(`.portfolio-item`, { interval: 100, delay: 700 });
+ScrollReveal().reveal(`.skills__content`, { delay: 670 });
+ScrollReveal().reveal(`.portfolio-item`, { interval: 100, delay: 500 });
